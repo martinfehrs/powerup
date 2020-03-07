cdbca7c19a1df7706b669e2dfc40c9e7d9fb9410
Martin Fehrs <martinfehrs@web.de>
2020-03-07 12:17:45 +0100

    Integration of catch2

--- test/cpp17_backports.cpp
+++ test/cpp17_backports.cpp
@@ -1,25 +1,11 @@
+#define CATCH_CONFIG_MAIN
+#include <catch.hpp>
+
 #include <powerup/cpp17_backports.hpp>
-#include <iostream>
+
 
 using namespace pup;
 
-PUP_STATIC_ASSERT(is_same_v<int, int > == std::is_same<int, int >::value);
-PUP_STATIC_ASSERT(is_same_v<int, char> == std::is_same<int, char>::value);
-
-PUP_STATIC_ASSERT(is_same_v<void_t<>,         void>);
-PUP_STATIC_ASSERT(is_same_v<void_t<int>,      void>);
-PUP_STATIC_ASSERT(is_same_v<void_t<int, int>, void>);
-
-PUP_STATIC_ASSERT(is_same_v<bool_constant<true >, std::integral_constant<bool, true >>);
-PUP_STATIC_ASSERT(is_same_v<bool_constant<false>, std::integral_constant<bool, false>>);
-
-PUP_STATIC_ASSERT(conjunction<                                >::value == true );
-PUP_STATIC_ASSERT(conjunction<std::true_type                  >::value == true );
-PUP_STATIC_ASSERT(conjunction<std::false_type                 >::value == false);
-PUP_STATIC_ASSERT(conjunction<std::false_type, std::false_type>::value == false);
-PUP_STATIC_ASSERT(conjunction<std::true_type , std::true_type >::value == true );
-PUP_STATIC_ASSERT(conjunction<std::true_type , std::false_type>::value == false);
-PUP_STATIC_ASSERT(conjunction<std::false_type , std::true_type>::value == false);
 
 struct non_copyable
 {
@@ -34,12 +20,42 @@ struct move_only
     move_only& operator=(move_only&&) = default;
 };
 
-PUP_STATIC_ASSERT(is_swappable<int >::value         == true );
-PUP_STATIC_ASSERT(is_swappable<void>::value         == false);
-PUP_STATIC_ASSERT(is_swappable<non_copyable>::value == false);
-PUP_STATIC_ASSERT(is_swappable<move_only>::value    == true );
 
-int main()
+TEST_CASE("testing is_same_v", "[is_same_v]")
 {
-    return 0;
+    REQUIRE(is_same_v<int, int > == std::is_same<int, int >::value);
+    REQUIRE(is_same_v<int, char> == std::is_same<int, char>::value);
 }
+
+TEST_CASE("testing void_t", "[void_t]")
+{
+    REQUIRE(is_same_v<void_t<>,         void>);
+    REQUIRE(is_same_v<void_t<int>,      void>);
+    REQUIRE(is_same_v<void_t<int, int>, void>);
+}
+
+TEST_CASE("testing bool_constant", "[bool_constant]")
+{
+    REQUIRE(is_same_v<bool_constant<true >, std::integral_constant<bool, true >>);
+    REQUIRE(is_same_v<bool_constant<false>, std::integral_constant<bool, false>>);
+}
+
+TEST_CASE("testing conjunction", "[conjunction]")
+{
+    REQUIRE(conjunction<                                >::value == true );
+    REQUIRE(conjunction<std::true_type                  >::value == true );
+    REQUIRE(conjunction<std::false_type                 >::value == false);
+    REQUIRE(conjunction<std::false_type, std::false_type>::value == false);
+    REQUIRE(conjunction<std::true_type , std::true_type >::value == true );
+    REQUIRE(conjunction<std::true_type , std::false_type>::value == false);
+    REQUIRE(conjunction<std::false_type , std::true_type>::value == false);
+}
+
+TEST_CASE("testing is_swappable", "[is_swappable]")
+{
+    REQUIRE(is_swappable<int >::value         == true );
+    REQUIRE(is_swappable<void>::value         == false);
+    REQUIRE(is_swappable<non_copyable>::value == false);
+    REQUIRE(is_swappable<move_only>::value    == true );
+}
+

552c6350999726e6a9f107fba36738c0a47fe78d
Martin Fehrs <martinfehrs@web.de>
2020-03-07 21:58:14 +0100

    Fixed wrong include path to catch2 contrib directory

--- test/legacy_iterator.cpp
+++ test/legacy_iterator.cpp
@@ -1,5 +1,9 @@
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch.hpp>
+
 #include <vector>
-#include "powerup/legacy_iterator.hpp"
+
+#include <powerup/legacy_iterator.hpp>
 
 using namespace pup;
 
@@ -17,52 +21,79 @@ struct custom_iterator
     custom_iterator& operator++();
 };
 
-static_assert(!has_iterator_traits_value_type        <not_an_iterator                    >::value);
-static_assert( has_iterator_traits_value_type        <int*                               >::value);
-static_assert( has_iterator_traits_value_type        <typename std::vector<int>::iterator>::value);
-static_assert( has_iterator_traits_value_type        <custom_iterator                    >::value);
+template <typename T>
+using iterator_t = typename T::iterator;
 
-static_assert(!has_iterator_traits_difference_type   <not_an_iterator                    >::value);
-static_assert( has_iterator_traits_difference_type   <int*                               >::value);
-static_assert( has_iterator_traits_difference_type   <typename std::vector<int>::iterator>::value);
-static_assert( has_iterator_traits_difference_type   <custom_iterator                    >::value);
 
-static_assert(!has_iterator_traits_reference         <not_an_iterator                    >::value);
-static_assert( has_iterator_traits_reference         <int*                               >::value);
-static_assert( has_iterator_traits_reference         <typename std::vector<int>::iterator>::value);
-static_assert( has_iterator_traits_reference         <custom_iterator                    >::value);
-
-static_assert(!has_iterator_traits_pointer           <not_an_iterator                    >::value);
-static_assert( has_iterator_traits_pointer           <int*                               >::value);
-static_assert( has_iterator_traits_pointer           <typename std::vector<int>::iterator>::value);
-static_assert( has_iterator_traits_pointer           <custom_iterator                    >::value);
-
-static_assert(!has_iterator_traits_iterator_category <not_an_iterator                    >::value);
-static_assert( has_iterator_traits_iterator_category <int*                               >::value);
-static_assert( has_iterator_traits_iterator_category <typename std::vector<int>::iterator>::value);
-static_assert( has_iterator_traits_iterator_category <custom_iterator                    >::value);
-
-static_assert(!has_iterator_traits                   <not_an_iterator                    >::value);
-static_assert( has_iterator_traits                   <int*                               >::value);
-static_assert( has_iterator_traits                   <typename std::vector<int>::iterator>::value);
-static_assert( has_iterator_traits                   <custom_iterator                    >::value);
-
-static_assert(!is_dereferenceable                    <not_an_iterator                    >::value);
-static_assert( is_dereferenceable                    <int*                               >::value);
-static_assert( is_dereferenceable                    <typename std::vector<int>::iterator>::value);
-static_assert( is_dereferenceable                    <custom_iterator                    >::value);
-
-static_assert(!is_pre_incrementable                  <not_an_iterator                    >::value);
-static_assert( is_pre_incrementable                  <int*                               >::value);
-static_assert( is_pre_incrementable                  <typename std::vector<int>::iterator>::value);
-static_assert( is_pre_incrementable                  <custom_iterator                    >::value);
-
-static_assert(!is_legacy_iterator                    <not_an_iterator                    >::value);
-static_assert( is_legacy_iterator                    <int*                               >::value);
-static_assert( is_legacy_iterator                    <typename std::vector<int>::iterator>::value);
-static_assert( is_legacy_iterator                    <custom_iterator                    >::value);
-
-int main()
+TEST_CASE("testing has_iterator_traits_value_type", "[has_iterator_traits_value_type]")
 {
-    return 0;
+    REQUIRE(!has_iterator_traits_value_type        <not_an_iterator             >::value);
+    REQUIRE( has_iterator_traits_value_type        <int*                        >::value);
+    REQUIRE( has_iterator_traits_value_type        <iterator_t<std::vector<int>>>::value);
+    REQUIRE( has_iterator_traits_value_type        <custom_iterator             >::value);
 }
+
+TEST_CASE("testing has_iterator_traits_difference_type", "[has_iterator_traits_difference_type]")
+{
+    REQUIRE(!has_iterator_traits_difference_type<not_an_iterator                    >::value);
+    REQUIRE( has_iterator_traits_difference_type<int*                               >::value);
+    REQUIRE( has_iterator_traits_difference_type<typename std::vector<int>::iterator>::value);
+    REQUIRE( has_iterator_traits_difference_type<custom_iterator                    >::value);
+}
+
+TEST_CASE("testing has_iterator_traits_reference", "[has_iterator_traits_reference]")
+{
+    REQUIRE(!has_iterator_traits_reference<not_an_iterator             >::value);
+    REQUIRE( has_iterator_traits_reference<int*                        >::value);
+    REQUIRE( has_iterator_traits_reference<iterator_t<std::vector<int>>>::value);
+    REQUIRE( has_iterator_traits_reference<custom_iterator             >::value);
+}
+
+TEST_CASE("testing has_iterator_traits_pointer", "[has_iterator_traits_pointer]")
+{
+    REQUIRE(!has_iterator_traits_pointer<not_an_iterator             >::value);
+    REQUIRE( has_iterator_traits_pointer<int*                        >::value);
+    REQUIRE( has_iterator_traits_pointer<iterator_t<std::vector<int>>>::value);
+    REQUIRE( has_iterator_traits_pointer<custom_iterator             >::value);
+}
+
+TEST_CASE("testing has_iterator_traits_iterator_category", "[has_iterator_traits_iterator_category]")
+{
+    REQUIRE(!has_iterator_traits_iterator_category<not_an_iterator             >::value);
+    REQUIRE( has_iterator_traits_iterator_category<int*                        >::value);
+    REQUIRE( has_iterator_traits_iterator_category<iterator_t<std::vector<int>>>::value);
+    REQUIRE( has_iterator_traits_iterator_category<custom_iterator             >::value);
+}
+
+TEST_CASE("testing has_iterator_traits", "[has_iterator_traits]")
+{
+    REQUIRE(!has_iterator_traits<not_an_iterator             >::value);
+    REQUIRE( has_iterator_traits<int*                        >::value);
+    REQUIRE( has_iterator_traits<iterator_t<std::vector<int>>>::value);
+    REQUIRE( has_iterator_traits<custom_iterator             >::value);
+}
+
+TEST_CASE("testing is_dereferenceable", "[is_dereferenceable]")
+{
+    REQUIRE(!is_dereferenceable<not_an_iterator             >::value);
+    REQUIRE( is_dereferenceable<int*                        >::value);
+    REQUIRE( is_dereferenceable<iterator_t<std::vector<int>>>::value);
+    REQUIRE( is_dereferenceable<custom_iterator             >::value);
+}
+
+TEST_CASE("testing is_pre_incrementable", "[is_pre_incrementable]")
+{
+    REQUIRE(!is_pre_incrementable<not_an_iterator             >::value);
+    REQUIRE( is_pre_incrementable<int*                        >::value);
+    REQUIRE( is_pre_incrementable<iterator_t<std::vector<int>>>::value);
+    REQUIRE( is_pre_incrementable<custom_iterator             >::value);
+}
+
+TEST_CASE("testing is_legacy_iterator", "[is_legacy_iterator]")
+{
+    REQUIRE(!is_legacy_iterator<not_an_iterator             >::value);
+    REQUIRE( is_legacy_iterator<int*                        >::value);
+    REQUIRE( is_legacy_iterator<iterator_t<std::vector<int>>>::value);
+    REQUIRE( is_legacy_iterator<custom_iterator             >::value);
+}
+
